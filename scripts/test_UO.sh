0399c60d60d35c82d62e9099b270156816afb720
YangChanhyeong <0814gerrardso@korea.ac.kr>
2025-10-20 20:33:08 +0900

    Modify bash file

---
+++ scripts/test_UO.sh
@@ -0,0 +1,28 @@
+# [ViT-L] 
+# --output-dir checkpoints/camera_NF_L
+# --clip_dir_vit checkpoints/pretrained_clip/ViT-B-16.pt
+
+# Training
+CUDA_VISIBLE_DEVICES=0,1 python main_tip_finetune.py --world-size 2 \
+    --pretrained checkpoints/detr-r50-hicodet.pth --output-dir checkpoints/camera_UO \
+    --num_classes 117 --port 3399 \
+    --clip_dir_vit checkpoints/pretrained_clip/ViT-B-16.pt --zs --use_insadapter \
+    --zs_type unseen_object \
+    --logits_type S+T \
+    --prompt_learning \
+    --use_verb_cue_selector \
+    --use_gaussian_sampler \
+    --use_cov_net \
+
+# Evaluation
+CUDA_VISIBLE_DEVICES=0 python main_tip_finetune.py --world-size 1 \
+    --pretrained checkpoints/detr-r50-hicodet.pth --output-dir checkpoints/camera_UO \
+    --num_classes 117 --port 3399 \
+    --clip_dir_vit checkpoints/pretrained_clip/ViT-B-16.pt --zs --use_insadapter \
+    --zs_type unseen_object \
+    --logits_type S+T \
+    --prompt_learning \
+    --use_verb_cue_selector \
+    --use_gaussian_sampler \
+    --use_cov_net \
+    --resume checkpoints/camera_UO/{File} --eval
